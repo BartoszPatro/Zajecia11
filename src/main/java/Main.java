ab977ec9532b9aef4384652baeda41140d576869
BartoszPatro <161605900+BartoszPatro@users.noreply.github.com>
2024-02-28 14:17:30 +0000

    C1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Hello");
     System.out.println("Cześć");
+    Systrm.out.println("Zmiana 2");
   }
 
   // @Test
